c9617e8cf9f546861bc6d5d758d2c749aa8f73d5
JuanHenaoParra <parjuag@gmail.com>
2019-09-20 17:21:24 -0500

    Cuadro mágico NxN corregido

--- Recursividad/MagicSquare.py
+++ Recursividad/MagicSquare.py
@@ -6,45 +6,39 @@ Created on Thu Sep 19 15:28:01 2019
 """
 
 global n
-n = 3
+n = 8
 
 board = [[0 for i in range(n)] for j in range(n)]
 
 # Step 3 - Validation
 def isValid(board, row, col, n):
     # To validate in the rows
-    for num in board[row]:
-        if(num == n):
-            return False
-    
+    if n in board[row]:
+        return False
     # To validate in the columns
     for r in range(len(board)):
         if(board[r][col] == n):
             return False
-    
     return True
 
 # Step - Recursive Def
-def SolveSquare(board, row):
+def SolveSquare(board, row, col):
     # Out Recursive Def
     if(row >= n):
         return True
-    
-    # In the row iterate in each column and put the options
-    for col in range(n):
-        for num in range(1, n+1):
-            if(isValid(board, row, col, num)):
-                board[row][col] = num
-                break
-    
-    # Continue in the next row
-    if(SolveSquare(board, row+1)):
-        return True
-
-    return False
-        
+    for num in range(1, n+1):
+        if(isValid(board, row, col, num)):
+            board[row][col] = num
+            if(col == n-1):
+                if(SolveSquare(board, row+1, 0)):
+                    return True
+            # Continue in the next col
+            elif(SolveSquare(board, row, col+1)):
+                return True
+            board[row][col] = 0
+            
 def printBoard(board):
-    if(SolveSquare(board, 0)):
+    if(SolveSquare(board, 0, 0)):
         for row in board:
             print(row)
     else:
