31c346042a6cdb591e6c8b70c94207dd2e9a40a7
JuanHenaoParra <parjuag@gmail.com>
2019-09-27 20:09:02 -0500

    Cambios estructurales

--- Recursividad/N-Queens.py
+++ Recursividad/N-Queens.py
@@ -2,60 +2,59 @@
 """
 Backtracking
 """
-n=4
+n = 4
 
-def imptablero (tablero):
-    for i in range(n): 
-        for j in range(n): 
-            print (tablero[i][j], end = " ") 
-        print()
-        
-        
-def asalvo (tablero, fila, columna):
-    for i in range(columna): 
-        if tablero[fila][i] == 1: 
-            return False
 
-    for i, j in zip(range(fila, -1, -1),       #Diagonal izq, derecha
-					range(columna, -1, -1)): 
-        if tablero[i][j] == 1: 
-            return False
+def imptablero(tablero):
+	for i in range(n):
+		for j in range(n):
+			print(tablero[i][j], end=' ')
+		print()
 
-    for i, j in zip(range(fila, n, 1),         #diagonal derecha, izq
-					range(columna, -1, -1)): 
-        if tablero[i][j] == 1: 
-            return False
-        
-    return True
 
-def solucion(tablero, columna): 
-	
-	if columna >= n:  #valida las columnas segun N
+def asalvo(tablero, fila, columna):
+	for i in range(columna):
+		if(tablero[fila][i] == 1):
+			return False
+
+	for i, j in zip(range(fila, -1, -1), range(columna, -1, -1)):  # Diagonal izq, derecha
+		if(tablero[i][j] == 1):
+			return False
+
+	for i, j in zip(range(fila, n, 1), range(columna, -1, -1)):  # diagonal derecha, izq
+		if(tablero[i][j] == 1):
+			return False
+
+	return True
+
+
+def solucion(tablero, columna):
+	if(columna >= n):  # valida las columnas segun N
 		return True
 
 	for i in range(n):
-		if asalvo(tablero, i, columna): 
+		if(asalvo(tablero, i, columna)):
 			tablero[i][columna] = 1
 
-			if solucion(tablero, columna + 1) == True:
+			if(solucion(tablero, columna + 1) is True):
 				return True
 
 			tablero[i][columna] = 0
 
 	return False
 
-def solucionNQ(): 
-	tablero = [ [0 for z in range(n)] for j in range(n)]
 
-	if solucion(tablero, 0) == False: 
-		print ("Solution does not exist")
+def solucionNQ(): 
+	tablero = [[0 for z in range(n)] for j in range(n)]
+
+	if(solucion(tablero, 0) is False):
+		print("Solution does not exist")
 		return False
 
 	imptablero(tablero)
 	return True
 
-# Driver Code 
-solucionNQ() 
-    
-        
-            
+# Driver Code
+
+
+solucionNQ()
